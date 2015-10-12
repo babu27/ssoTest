d0d93c4886233cc052d78a9359fe984f64b75d98
Bachcha Babu Sah <bsah@vmware.com>
2015-10-12 15:04:41 -0400

    added comment

---
+++ ssoTest/Controllers/Api/RestController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Http;
+
+namespace ssoTest.Controllers.Api
+{
+    public class RestController : ApiController
+    {
+        //// GET api/rest
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/rest/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+      
+
+        // PUT api/rest/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/rest/5
+        public void Delete(int id)
+        {
+        }
+
+        // ssoauth  api/rest/ssoauthGet
+        
+        [HttpGet]
+        [ActionName("ssoauthGet")]
+        public object ssoauthGet(int? id=null)
+        {
+            return new {AdminUserId = "p", Result = "0000"};
+        }
+
+        [HttpPost]
+        public object ssoauth([FromBody] string ssoToken)
+        {
+            return new { AdminUserId = "p", Result = "0000" };
+        }
+    }
+}
